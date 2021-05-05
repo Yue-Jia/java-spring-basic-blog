e99f2c96c2f5ffd05c237124c9fc2e5fe3618702
yue jia <yuejia070@gmail.com>
2021-05-05 00:21:15 -0400

    add body

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,5 +20,11 @@ public class BlogController {
         List<Post> lp = postRepository.getAllPosts();
         model.put("posts",lp);
         return "home";
+    }
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap){
+        Post post= postRepository.findById(id);
+        modelMap.put("post",post);
+        return "post-details";
     }
 }
